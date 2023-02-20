d04cbb09e1e6da76565c1e492982432e74f30036
mojtaba kalhor <mj13kalhor@gmail.com>
2023-02-20 19:02:55 +0330

    add requirements.txt and custom admin model

--- blog/models.py
+++ blog/models.py
@@ -13,29 +13,27 @@ def validate_file_extension(value):
 
 
 
-class userprofile(models.Model):
+class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.FileField(upload_to='files/user_avatar', null=False, blank=False)
     description = models.CharField(max_length=512, null=False, blank=False)
 
     def __str__(self):
-        return self.user.first_name + ' ' + self.user.last_name
-
+        return self.user.first_name
 class Article(models.Model):
     title = models.CharField(max_length=128, null=False, blank=False)
     cover = models.FileField(upload_to='files/article_cover/', null=False, blank=False)
     content = RichTextField()
     created_at = models.DateTimeField(default=datetime.now, blank=False)
-    category = models.ForeignKey('category', on_delete=models.CASCADE)
-    author = models.OneToOneField(userprofile, on_delete=models.CASCADE)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    author = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
 
 
-    def __str__(self):
-        return self.title
+
 class Category(models.Model):
     title = models.CharField(max_length=128, null=True, blank=True)
     cover = models.FileField(upload_to='files/category_cover/', null=False, blank=False)
 
-
     def __str__(self):
         return self.title
+
